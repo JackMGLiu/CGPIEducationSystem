a7fcb2ef80ab7ae448415b2ff629ae9dbbae08f3
Jack-Liu <love_zs1314@126.com>
2017-10-11 11:38:08 +0800

    cgpi

---
+++ src/CGPI2Solution/QJ.Framework.Infrastructure/Core/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QJ.Framework.Infrastructure.Core
+{
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, Column("Guid"),MaxLength(36)]
+        public string GuidCode { get; set; }
+
+        /// <summary>
+        /// 启用状态
+        /// </summary>
+        [Required]
+        public bool IsEnableed { get; set; }
+
+        /// <summary>
+        /// 删除状态
+        /// </summary>
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ src/CGPI2Solution/QJ.Framework.Infrastructure/NLog/OperationLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NLog;
+
+namespace MG.Infrastructure.NLog
+{
+    public class SysLogger
+    {
+        readonly static Logger operLogger = LogManager.GetLogger("operlogger");
+
+        public static void Info(int type,string operUserName, string operIp, string content)
+        {
+            LogEventInfo theEvent = new LogEventInfo(LogLevel.Info, "", content);
+            theEvent.Properties["Type"] = type;
+            theEvent.Properties["OperUser"] = operUserName;
+            theEvent.Properties["IP"] = operIp;
+            operLogger.Log(theEvent);
+        }
+
+        public static void Error(int type,string operUserName, string operIp, string content,Exception ex)
+        {
+            LogEventInfo theEvent = new LogEventInfo(LogLevel.Error, "", null, content, null, ex);
+            theEvent.Properties["Type"] = type;
+            theEvent.Properties["OperUser"] = operUserName;
+            theEvent.Properties["IP"] = operIp;
+            operLogger.Log(theEvent);
+        }
+
+        public static void Flush()
+        {
+            LogManager.Flush();
+        }
+    }
+}
